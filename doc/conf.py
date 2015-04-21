f84e9b7a71cf2674ee319ca1fa2b3e3cb743ae38
J Forde <jzf2101@columbia.edu>
2015-04-21 17:15:12 -0400

    closes #25

--- doc/conf.py
+++ doc/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'datamicroscopes'
-copyright = u'2014, Stephen Tu and Eric Jonas'
+copyright = u'2014, Qadium'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -201,8 +201,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'datamicroscopes.tex', u'datamicroscopes Documentation',
-   u'Stephen Tu and Eric Jonas', 'manual'),
+  ('index', 'datamicroscopes.tex', 'datamicroscopes Documentation',
+   'Qadium', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -231,8 +231,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'datamicroscopes', u'datamicroscopes Documentation',
-     [u'Stephen Tu and Eric Jonas'], 1)
+    ('index', 'datamicroscopes', 'datamicroscopes Documentation',
+     ['Qadium'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -245,8 +245,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'datamicroscopes', u'datamicroscopes Documentation',
-   u'Stephen Tu and Eric Jonas', 'datamicroscopes', 'One line description of project.',
+  ('index', 'datamicroscopes', 'datamicroscopes Documentation',
+   'Qadium', 'datamicroscopes', 'One line description of project.',
    'Miscellaneous'),
 ]
 

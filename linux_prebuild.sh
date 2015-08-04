6295ec5e527e5bf5929f4350ff9e2fa47f4d73da
datamicroscopes-travis-builder <datamicroscopes.travis.builder@gmail.com>
2015-08-04 12:31:36 -0400

    Fix miniconda install

--- linux_prebuild.sh
+++ linux_prebuild.sh
@@ -8,7 +8,7 @@ export CC=gcc-4.8
 export CXX=g++-4.8
 wget http://repo.continuum.io/miniconda/Miniconda-3.5.5-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
-"./miniconda.sh -b"
+./miniconda.sh -b
 export PATH=/home/travis/miniconda/bin:$PATH
 conda update --yes conda
 conda install --yes conda-server conda-build jinja2 anaconda-client sh

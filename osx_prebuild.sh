711105cac6c0e89a4417490a3f63364d702e313a
Tim Hopper <tdhopper@gmail.com>
2015-08-04 16:59:37 -0400

    Fix comments

--- osx_prebuild.sh
+++ osx_prebuild.sh
@@ -1,7 +1,7 @@
 gcc -v
 c++ -v
-XXX(stephentu): HACK
-see https://github.com/datamicroscopes/common/issues/1
+# XXX(stephentu): HACK
+# see https://github.com/datamicroscopes/common/issues/1
 sudo mv `which gcc-4.2` `which gcc-4.2`-old
 wget http://repo.continuum.io/miniconda/Miniconda-3.5.5-MacOSX-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh

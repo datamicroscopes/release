ed1584e86f2811fc435d40aebfb53117abd3db56
Tim Hopper <tdhopper@gmail.com>
2015-08-05 10:33:05 -0400

    Add fabfile for deploying to linux/os x

---
+++ fabfile.py
@@ -0,0 +1,31 @@
+import sys
+import sh
+
+from fabric import api as fab
+
+sed = sh.sed.bake('-i bak -e')
+TRAVIS_YAML = '.travis.yml'
+REPLACE_LANGUAGE = 's/language: .*/language: {}/'
+
+
+def is_dirty():
+    return "" != sh.git.status(porcelain=True).strip()
+
+
+def release(language, message):
+    if is_dirty():
+        sys.exit("Repo must be in clean state before deploying. Please commit changes.")
+    sed(REPLACE_LANGUAGE.format(language), TRAVIS_YAML)
+    if is_dirty():
+        sh.git.add(TRAVIS_YAML)
+        sh.git.commit(m="'{}'".format(message))
+
+
+@fab.task
+def release_osx():
+    release('objective-c', "Release OS X")
+
+
+@fab.task
+def release_linux():
+    release('python', "Release Linux")

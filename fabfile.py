9a8938cc402958b7124a6949060aac701e37b269
Tim Hopper <tdhopper@gmail.com>
2015-08-05 15:16:09 -0400

    Add channel option and generic channel task

--- fabfile.py
+++ fabfile.py
@@ -12,7 +12,7 @@ def is_dirty():
     return "" != sh.git.status(porcelain=True).strip()
 
 
-def release(language, message):
+def _release(language, message):
     if is_dirty():
         sys.exit("Repo must be in clean state before deploying. Please commit changes.")
     sed(REPLACE_LANGUAGE.format(language), TRAVIS_YAML)
@@ -38,10 +38,18 @@ def update():
 
 
 @fab.task
-def release_osx():
-    release('objective-c', "Release OS X")
+def release(os, channel="main"):
+    if os.lower() == "osx":
+        _release('objective-c', "Release OS X", channel)
+    if os.lower() == "linux":
+        _release('python', "Release Linux", channel)
 
 
 @fab.task
-def release_linux():
-    release('python', "Release Linux")
+def release_osx(channel="main"):
+    release("osx", channel)
+
+
+@fab.task
+def release_linux(channel="main"):
+    release("linux", channel)

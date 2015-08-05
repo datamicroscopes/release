9acd11e0a7d0e7b476cb51d1c5e07bf0cb2cbf68
Tim Hopper <tdhopper@gmail.com>
2015-08-05 09:06:07 -0400

    Fix another bug

--- build_docs.sh
+++ build_docs.sh
@@ -2,8 +2,7 @@ conda install --yes microscopes-common microscopes-mixturemodel microscopes-irm 
 conda install --yes sphinx pip
 which pip
 pip install numpydoc
-git clone "https://datamicroscopes-travis-builder:$GITHUB_PASSWORD@github.com/datamicroscopes/datamicroscopes.github.io"
-  D
+git clone "https://datamicroscopes-travis-builder:$GITHUB_PASSWORD@github.com/datamicroscopes/datamicroscopes.github.io" D
 (cd doc && make html)
 rm -rf D/*
 cp -R doc/_build/html/* D

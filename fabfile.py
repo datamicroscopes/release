0c05ba9d01cbfdf5b924d658ca2acd55feed36bf
Tim Hopper <tdhopper@gmail.com>
2015-08-05 15:19:52 -0400

    Fix channel modification

--- fabfile.py
+++ fabfile.py
@@ -6,21 +6,23 @@ from fabric import api as fab
 sed = sh.sed.bake('-i bak -e')
 TRAVIS_YAML = '.travis.yml'
 REPLACE_LANGUAGE = 's/language: .*/language: {}/'
+REPLACE_CHANNEL = 's/ANACONDA_CHANNEL=.*/ANACONDA_CHANNEL={}/'
 
 
 def is_dirty():
     return "" != sh.git.status(porcelain=True).strip()
 
 
-def _release(language, message):
+def _release(language, message, channel):
     if is_dirty():
         sys.exit("Repo must be in clean state before deploying. Please commit changes.")
     sed(REPLACE_LANGUAGE.format(language), TRAVIS_YAML)
+    sed(REPLACE_CHANNEL.format(channel), TRAVIS_YAML)
     if is_dirty():
         sh.git.add(TRAVIS_YAML)
-    sh.git.commit(m=message, allow_empty=True)
-    sh.git.pull(rebase=True)
-    sh.git.push()
+    # sh.git.commit(m=message, allow_empty=True)
+    # sh.git.pull(rebase=True)
+    # sh.git.push()
 
 
 @fab.task
@@ -41,8 +43,10 @@ def update():
 def release(os, channel="main"):
     if os.lower() == "osx":
         _release('objective-c', "Release OS X", channel)
-    if os.lower() == "linux":
+    elif os.lower() == "linux":
         _release('python', "Release Linux", channel)
+    else:
+        sys.exit("Specify either osx or linux.")
 
 
 @fab.task

ab2a409063bc195771ff1e4a63823536515e325e
J Forde <jzf2101@columbia.edu>
2015-10-07 15:27:40 -0400

    updating conf to switch redering to mathjax

--- doc/conf.py
+++ doc/conf.py
@@ -32,10 +32,11 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.pngmath',
+    'sphinx.ext.mathjax',
     'numpydoc',
+    'IPython.sphinxext.ipython_directive',
+    'IPython.sphinxext.ipython_console_highlighting',
 ]
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
